c5c15ef3b8060feb93c1143e88ee4c7b6ac98e67
TobyTowler <tobytowler11@gmail.com>
2024-05-06 22:38:24 +0100

    barbar nvim

--- dotsUpdate.sh
+++ dotsUpdate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp -r "/home/toby/.config/nvim" .
-cp -r "/home/toby/.config/kitty" . 
-cp -r "/home/toby/.config/neofetch" .
-cp "/home/toby/.bashrc" .
-cp -r "/home/toby/.config/hypr" "./hyprland"
-cp -r "/home/toby/.config/wofi" "./hyprland"
-cp -r "/home/toby/.config/waybar" "./hyprland"
+cp -r "/home/Toby/.config/nvim" .
+cp -r "/home/Toby/.config/kitty" . 
+cp -r "/home/Toby/.config/neofetch" .
+cp "/home/Toby/.bashrc" .
+cp -r "/home/Toby/.config/hypr" "./hyprland"
+cp -r "/home/Toby/.config/wofi" "./hyprland"
+cp -r "/home/Toby/.config/waybar" "./hyprland"
