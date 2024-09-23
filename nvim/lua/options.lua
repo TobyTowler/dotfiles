87418cfbf4e74f5e1b24803eec6e1d587f1b574d
Toby Towler <tobytowler11@gmail.com>
2024-09-23 15:27:21 +0100

    waybar date and nvim scroll off

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,4 +1,4 @@
-vim.o.tabline = 4
+vim.o.tabline = "4"
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.opt.expandtab = true
@@ -6,7 +6,7 @@ vim.o.smarttab = true
 vim.o.softtabstop = 4
 vim.o.smartindent = true
 
-vim.o.scrolloff = 15
+vim.o.scrolloff = 8
 
 --line numbers
 vim.o.nu = true
@@ -16,7 +16,7 @@ vim.g.autoformat = true
 
 -- Taken from Lazyvim
 vim.opt.cursorline = true
-vim.opt.confirm = true -- Confirm to save changes before exiting modified buffere
+vim.opt.confirm = true    -- Confirm to save changes before exiting modified buffere
 vim.opt.ignorecase = true -- Ignore case
 vim.opt.expandtab = true
 
