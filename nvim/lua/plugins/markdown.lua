696d1919bba36d12334a769bfe968b7c2c8826d0
Toby Towler <tobytowler11@gmail.com>
2025-01-30 11:24:01 +0000

    nvim bulletpoints

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,25 +1,29 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    opts = {},
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    config = function()
-        require('render-markdown').setup({
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {},
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        config = function()
+            require('render-markdown').setup({
 
-            file_types = { 'markdown', 'quarto' },
-            render_modes = true,
-            heading = {
-                backgrounds = { '' },
-                icons = { '<> ' },
-                -- border = true,
-                -- below = '‾',
-                -- above = '',
-            },
+                file_types = { 'markdown', 'quarto' },
+                render_modes = true,
+                heading = {
+                    backgrounds = { '' },
+                    icons = { '<> ' },
+                    -- border = true,
+                    -- below = '‾',
+                    -- above = '',
+                },
 
-            bullet = {
-                left_pad = 1,
-            }
+                bullet = {
+                    left_pad = 1,
+                }
 
 
-        })
-    end
+            })
+        end
+    },
+    -- { "preservim/vim-markdown" },
+    { "bullets-vim/bullets.vim" },
 }
